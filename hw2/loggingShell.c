a33d9b7aa2d2d21f9e8f1c3b5e794cedc2d7812b
lukerobbins2112 <lukerobbins2112@gmail.com>
2019-05-05 17:23:25 -0500

    Added a command loop to process input, and a function to run those commands. Right now the loop seems to work, and the system exits if 'exit' is given as command. Still need to test the signal handling

--- hw2/loggingShell.c
+++ hw2/loggingShell.c
@@ -3,9 +3,43 @@
 #include <stdio.h>
 #include <string.h>
 
-const char * USAGE = "USAGE loggingShell [logfile]\n";
+#include <signal.h>
+#include <sys/types.h>
 
+#define BUF_SIZE 1024
+
+//----------------------------------------------------------------
+// Global Data
+//----------------------------------------------------------------
+
+const char * USAGE = "USAGE loggingShell [logfile]\n";
 int logFD;
+
+int readyToExit = 0;
+int curChildPID = -1;
+
+//----------------------------------------------------------------
+// Function declarations
+//----------------------------------------------------------------
+
+void logString(const char * msg, size_t bytes);
+
+//----------------------------------------------------------------
+// Functions
+//----------------------------------------------------------------
+
+static void sigIntHandler(int sig){
+
+  if (curChildPID == -1){
+    printf("No child process executing\n");
+    return;
+  }
+
+  // Send termination signal to child process
+  logString("Terminating child process %d\n", curChildPID);
+  kill(curChildPID, SIGINT);
+
+}
 
 void logString(const char * msg, size_t bytes){
 
@@ -14,11 +48,31 @@ void logString(const char * msg, size_t bytes){
 
 }
 
+void processCmd(const char * cmd, ssize_t len){
+
+  logString("Processing command: ", 20);
+  logString(cmd, len);
+  logString("\n", 1);
+
+
+  if (strcmp(cmd, "exit") == 0){
+    readyToExit = 1;
+  }
+
+}
+
 int main(int argc, char **argv){
 
   if (argc < 2){
     printf("%s", USAGE);
     return 0;
+  }
+
+  // Set up signal handler
+
+  if (signal(SIGINT, sigIntHandler) == SIG_ERR){
+    perror("signal");
+    return -1;
   }
 
   // Create file to be used for logging
@@ -32,7 +86,26 @@ int main(int argc, char **argv){
     return -1;
   }
 
-  logString("test log\n", 9);
+  // Loop while commands are given, and execute commands in child processes
+
+  char input[BUF_SIZE];
+  ssize_t bytesRead;
+
+  while(readyToExit == 0){
+
+    printf("$ ");
+
+    bytesRead = read(STDIN_FILENO, input, BUF_SIZE - 1);
+    input[bytesRead-1] = '\0';
+    processCmd(input, bytesRead);
+
+  }
+
+  // CLose log file
+  if (close(logFD) == -1){
+    perror("close");
+    return -1;
+  }
 
   return 0;
 
