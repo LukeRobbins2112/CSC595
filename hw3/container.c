41457084bc2976be26185a97d53fa350cd6be686
lukerobbins2112 <lukerobbins2112@gmail.com>
2019-05-28 11:01:36 -0500

    Added command line processing of arguments and packing into struct, along with printing validation

--- hw3/container.c
+++ hw3/container.c
@@ -9,7 +9,18 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <stdlib.h>
 #define STACKSIZE (1024 * 1024)
+
+struct params{
+
+  char * cmd;
+  char ** argv;
+  int cpu_pct;
+  int mem_limit;
+  int num_levels;
+
+};
 
 
 static char stack[STACKSIZE];
@@ -41,7 +52,7 @@ int exec(void * args)
     // Create a message queue
     // USe print_err to print error if creating message queue fails
     result = msgget(0, IPC_CREAT | 0666);
-    if (result != 0){
+    if (result < 0){
       print_err("msgget");
     }
 
@@ -61,6 +72,37 @@ int main(int argc, char ** argv)
         fprintf(stderr, "No command specified\n");
         return 1;
     }
+
+
+    // Initialize params struct
+    struct params userParams;
+    userParams.cmd = (char *)malloc(strlen(argv[1]) + 1);
+    strcpy(userParams.cmd, argv[1]);
+
+    userParams.cpu_pct = atoi(argv[2]);
+    userParams.mem_limit = atoi(argv[3]);
+    userParams.num_levels = atoi(argv[4]);
+
+    // Fill in additional args
+    int numArgs = argc - 5;
+    if (numArgs > 0){
+      userParams.argv = (char **)malloc(sizeof(char *) * numArgs);
+    }
+
+    for (int i = 5; i < argc; i++){
+      userParams.argv[i-5] = (char *)malloc(strlen(argv[i]) + 1);
+      strcpy(userParams.argv[i - 5], argv[i]);
+    }
+
+    printf("cmd: %s\n", userParams.cmd);
+    printf("cpu_pct: %d\n", userParams.cpu_pct);
+    printf("mem_limit: %d\n", userParams.mem_limit);
+    printf("num_levels: %d\n", userParams.num_levels);
+
+    for (int i = 0; i < numArgs; i++){
+      printf("Arg #%d: %s\n", i, userParams.argv[i]);
+    }
+
     // Namespace flags
     // ADD more flags
     const int flags = SIGCHLD | CLONE_NEWNS | CLONE_NEWPID | CLONE_NEWNET | CLONE_NEWIPC | CLONE_NEWUTS | CLONE_NEWUSER;
